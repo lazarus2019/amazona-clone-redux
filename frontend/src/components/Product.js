e01c084f98adaf8d87c90fa501787417afe8d211
NTSon <thaisonbk2020@gmail.com>
2023-05-14 15:26:30 +0700

    update 14/05/23

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,27 @@
+import Rating from "./Rating";
+import { Link } from "react-router-dom";
+
+function Product(props) {
+  const { product } = props;
+
+  return (
+    <div key={product._id} className="card">
+      <Link to={`/product/${product._id}`}>
+        <img className="medium" src={product.image} alt={product.name} />
+      </Link>
+      <div className="card-body">
+        <Link to={`/product/${product._id}`}>
+          <h2>{product.name}</h2>
+        </Link>
+        <Rating
+          rating={product.rating}
+          caption=""
+          numReviews={product.numReviews}
+        />
+        <div className="price">${product.price}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Product;
---
+++ frontend/src/redux/actions/cartActions.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+import { CART_ADD_ITEM } from "../constants/cartConstants";
+
+export const addToCart = (productId, qty) => async (dispatch, getState) =>{
+    const {data}
+}
